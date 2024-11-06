4a46ed02f173cf0446204b0a8755a2e11ef75545
Dominika Góral <dominika.goral@inetum.com>
2024-11-06 16:13:45 +0100

    feat: add tab switcher

---
+++ components/editor/panels/TabSwitcher.ts
@@ -0,0 +1,48 @@
+import type { PanelProperties } from "grapesjs";
+import { Paintbrush, Settings, Layers, Blocks } from "lucide-static";
+
+export const TabSwitcher = (): PanelProperties => {
+  const { t } = useI18n();
+
+  return {
+    id: "tab-switcher",
+    buttons: [
+      {
+        id: "style-component",
+        label: Paintbrush,
+        command: "open-sm",
+        active: true,
+        togglable: false,
+        attributes: { id: "style-component", title: t("styleComponent") },
+        tagName: "button",
+      },
+      {
+        id: "edit-component-settings",
+        label: Settings,
+        command: "open-tm",
+        togglable: false,
+        attributes: {
+          id: "edit-component-settings",
+          title: t("editComponentSettings"),
+        },
+        tagName: "button",
+      },
+      {
+        id: "view-layers",
+        label: Layers,
+        command: "open-layers",
+        togglable: false,
+        attributes: { title: t("viewLayers") },
+        tagName: "button",
+      },
+      {
+        id: "add-blocks",
+        label: Blocks,
+        command: "open-blocks",
+        togglable: false,
+        attributes: { title: t("addBlocks") },
+        tagName: "button",
+      },
+    ],
+  };
+};
